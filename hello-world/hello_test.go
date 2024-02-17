5e339c275160410783b576fa549a0dd9d196143d
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2024-02-16 23:33:28 -0300

    feat: add parameter to Hello function

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	output := Hello("John Doe")
-	expected := "Hello, John Doe"
+	t.Run("saying hello to people", func(t *testing.T) {
+		output := Hello("John Doe")
+		expected := "Hello, John Doe"
 
-	if output != expected {
-		t.Errorf("output %q expected %q", output, expected)
-	}
+		if output != expected {
+			t.Errorf("output %q expected %q", output, expected)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		output := Hello("")
+		expected := "Hello, World"
+
+		if output != expected {
+			t.Errorf("output %q expected %q", output, expected)
+		}
+	})
 }
