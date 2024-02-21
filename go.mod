73622bcda3934316ea13cbc5796206d73cfeb42a
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2024-02-20 22:50:38 -0300

    feat: add integers chapter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module helloworld
+module go-learn
 
 go 1.22.0
---
+++ integers/sum_test.go
@@ -0,0 +1,26 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Sum(x, y int) int {
+	return x + y
+}
+
+func ExampleSum() {
+	sum := Sum(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+// Sum takes two integers and returns the sum of them.
+func TestSum(t *testing.T) {
+	sum := Sum(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
