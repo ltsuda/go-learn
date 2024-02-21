7deba4f3a9e661b3fa4109727480935904673ea0
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2024-02-20 23:26:43 -0300

    refactor: add example and max repeat count

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,9 +2,15 @@ package iteration
 
 const repeatCount = 5
 
-func Repeat(character string) string {
+func Repeat(character string, maxCount int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	var maxRepeat int = repeatCount
+
+	if maxCount > 0 {
+		maxRepeat = maxCount
+	}
+
+	for i := 0; i < maxRepeat; i++ {
 		repeated += character
 	}
 
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,10 +1,13 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("z")
-	expected := "zzzzz"
+	repeated := Repeat("z", 10)
+	expected := "zzzzzzzzzz"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
@@ -13,6 +16,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("z")
+		Repeat("z", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeatChar := Repeat("a", 5)
+	fmt.Println(repeatChar)
+	// Output: aaaaa
 }
