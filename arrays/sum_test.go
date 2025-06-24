f8de2c35311ed3d8a80295e2b1adbf22cadce149
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-06-23 23:09:13 -0300

    refactor: remove similar test
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,16 +4,6 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
-		sum := Sum(numbers)
-		expected := 15
-
-		if sum != expected {
-			t.Errorf("expected %d but got %d, %v", expected, sum, numbers)
-		}
-	})
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
