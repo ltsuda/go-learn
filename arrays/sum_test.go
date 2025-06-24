7387e2fb580f347eb439dcc9d50b27c13a628b6d
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-06-23 23:42:54 -0300

    refactor: sum when empty slice and assert func
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -29,10 +29,25 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	sum := SumAllTails([]int{1, 2}, []int{0, 9})
-	expected := []int{2, 9}
 
-	if !reflect.DeepEqual(sum, expected) {
-		t.Errorf("expected %v but got %v", expected, sum)
+	checkSums := func(t testing.TB, result, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("expected %v but got %v", expected, result)
+		}
 	}
+
+	t.Run("make the sums o f some slices", func(t *testing.T) {
+
+		sum := SumAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+
+		checkSums(t, sum, expected)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		sum := SumAllTails([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+
+		checkSums(t, sum, expected)
+	})
 }
