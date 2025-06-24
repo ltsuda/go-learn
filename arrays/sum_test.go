132ba95c033967ccb91eb27e5ed6106d6d747c90
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-06-23 23:07:22 -0300

    refactor: use slices in arrays module
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,12 +3,25 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	sum := Sum(numbers)
-	expected := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if sum != expected {
-		t.Errorf("expected %d but got %d, %v", expected, sum, numbers)
-	}
+		sum := Sum(numbers)
+		expected := 15
+
+		if sum != expected {
+			t.Errorf("expected %d but got %d, %v", expected, sum, numbers)
+		}
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		sum := Sum(numbers)
+		expected := 6
+
+		if sum != expected {
+			t.Errorf("expected %d but got %d, %v", expected, sum, numbers)
+		}
+	})
 }
