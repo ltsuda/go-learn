03e3834b9de8d32ce624f87a1f91a839e244f978
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-06-23 23:02:23 -0300

    feat: initial arrays
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

---
+++ arrays/sum_test.go
@@ -0,0 +1,14 @@
+package arrays
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	sum := Sum(numbers)
+	expected := 15
+
+	if sum != expected {
+		t.Errorf("expected %d but got %d, %v", expected, sum, numbers)
+	}
+}
