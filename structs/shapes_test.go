4e806a77c32ea10cb579972260baf16b415fa9a3
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-06-25 22:38:49 -0300

    refactor: use sub tests with naming
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,20 +15,22 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name     string
 		shape    Shape
 		expected float64
 	}{
-		{shape: Rectangle{Width: 10.0, Height: 12.0}, expected: 120},
-		{shape: Circle{Radius: 10.0}, expected: 314.1592653589793},
-		{shape: Triangle{Base: 10, Height: 8}, expected: 40.0},
+		{name: "Rectangle", shape: Rectangle{Width: 10.0, Height: 12.0}, expected: 120},
+		{name: "Circle", shape: Circle{Radius: 10.0}, expected: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 10, Height: 8}, expected: 40.0},
 	}
 
 	for _, tt := range areaTests {
-		area := tt.shape.Area()
+		t.Run(tt.name, func(t *testing.T) {
+			area := tt.shape.Area()
 
-		if area != tt.expected {
-			t.Errorf("expected %g, got %g", tt.expected, area)
-		}
+			if area != tt.expected {
+				t.Errorf("expected %g, got %g", tt.expected, area)
+			}
+		})
 	}
-
 }
