1dc95f13e38bcd562d1dff05175c2c445a16726f
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-06-27 23:38:34 -0300

    refactor: implement errors handling
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -12,12 +15,24 @@ type Stringer interface {
 	String() string
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) WithDraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
 
 func (b Bitcoin) String() string {
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,13 +5,57 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(25)}
+		err := wallet.WithDraw(Bitcoin(10))
+
+		assertNoError(t, err)
+		assertBalance(t, wallet, Bitcoin(15))
+	})
+
+	t.Run("withdraw with insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.WithDraw(Bitcoin(50))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
+	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, expected Bitcoin) {
+	t.Helper()
 
 	result := wallet.Balance()
-	expected := Bitcoin(10)
 
 	if result != expected {
 		t.Errorf("expected %s got %s", expected, result)
+	}
+}
+
+func assertError(t testing.TB, result error, expected error) {
+	t.Helper()
+
+	if result == nil {
+		t.Fatal("expected an error but didn't get one")
+	}
+
+	if result != expected {
+		t.Errorf("expected %q, got %q", expected, result)
+	}
+}
+
+func assertNoError(t testing.TB, result error) {
+	t.Helper()
+	if result != nil {
+		t.Fatal("got an error but didn't expect one")
 	}
 }
