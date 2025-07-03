fe701025e84fb8be4cb3008126a213191a433d5f
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-07-02 22:14:18 -0300

    refactor: implement Add method
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -5,8 +5,30 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	result := dictionary.Search("test")
+	t.Run("known word", func(t *testing.T) {
+		result, _ := dictionary.Search("test")
+		expected := "this is just a test"
+		assertStrings(t, result, expected)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertError(t, err, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
 	expected := "this is just a test"
+	result, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
 
 	assertStrings(t, result, expected)
 }
@@ -15,5 +37,12 @@ func assertStrings(t testing.TB, result, expected string) {
 	t.Helper()
 	if result != expected {
 		t.Errorf("expected %q got %q", expected, result)
+	}
+}
+
+func assertError(t testing.TB, result, expected error) {
+	t.Helper()
+	if result != expected {
+		t.Errorf("expected %q got %q", expected.Error(), result)
 	}
 }
