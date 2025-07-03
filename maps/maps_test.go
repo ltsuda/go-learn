9f8a4ecbebd957ebe33d5b74d3a7a2addc5c8b1b
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-07-02 21:47:00 -0300

    refactor: create custom Dictionary type from map
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,12 +3,17 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	result := Search(dictionary, "test")
+	result := dictionary.Search("test")
 	expected := "this is just a test"
 
+	assertStrings(t, result, expected)
+}
+
+func assertStrings(t testing.TB, result, expected string) {
+	t.Helper()
 	if result != expected {
-		t.Errorf("expected %q got %q given, %q", expected, result, "test")
+		t.Errorf("expected %q got %q", expected, result)
 	}
 }
