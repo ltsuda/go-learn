4335a9466ea5ce38033bc8efb21fe28e282426a7
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-07-15 22:39:30 -0300

    refactor: implement concurrent increment with WaitGroup
    Signed-off-by: Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,21 +1,41 @@
-package sync
+package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it as 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
 	})
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		expected := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(expected)
+
+		for i := 0; i < expected; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, expected)
+	})
 }
 
-func assertCounter(t testing.TB, result Counter, expected int) {
+func assertCounter(t testing.TB, result *Counter, expected int) {
 	t.Helper()
 	if result.Value() != expected {
-		t.Errorf("expected %d, got %d", result.Value(), 3)
+		t.Errorf("expected %d, got %d", expected, result.Value())
 	}
 }
